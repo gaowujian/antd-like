c2bdfd4c1d9b175e24a88a31ddc00fdc036fe28d
tony <806003097@qq.com>
2021-11-01 22:55:40 +0800

    初始化项目并指定色彩和开始需要的变量

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>11</h1>
+      <h2>22</h2>
+      <h3>333</h3>
+      <p>paragraph</p>
     </div>
   );
 }
