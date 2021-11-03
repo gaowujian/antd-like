085d523e06c765b6637f51b534b67998202b59ca
tony <806003097@qq.com>
2021-11-03 22:08:00 +0800

    完成类型和尺寸的基本按钮

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ function App() {
     <div className="App container">
       <Button>按钮文字</Button>
       <Button btnType="primary">主要按钮</Button>
+      <Button btnType="secondary">主要按钮</Button>
       <Button size="lg">大号按钮</Button>
-      <Button disabled>禁用按钮</Button>
+      <Button size="sm">小号按钮</Button>
+      {/* <Button disabled>禁用按钮</Button>
       <Button btnType="link" href="www.baidu.com">
         Link按钮
-      </Button>
+      </Button> */}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,43 @@
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3498db",
+        secondary: "#ecf0f1",
+        success: "#2ecc71",
+        info: "#2980b9",
+        warning: "#e67e22",
+        danger: "#e74c3c",
+        light: "#EEEEEE",
+        dark: "#111111",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
 };
+
+// $blue:    #0d6efd !default;
+// $indigo:  #6610f2 !default;
+// $purple:  #6f42c1 !default;
+// $pink:    #d63384 !default;
+// $red:     #dc3545 !default;
+// $orange:  #fd7e14 !default;
+// $yellow:  #fadb14 !default;
+// $green:   #52c41a !default;
+// $teal:    #20c997 !default;
+// $cyan:    #17a2b8 !default;
+
+// $primary:       $blue !default;
+// $secondary:     $gray-600 !default;
+// $success:       $green !default;
+// $info:          $cyan !default;
+// $warning:       $yellow !default;
+// $danger:        $red !default;
+// $light:         $gray-100 !default;
+// $dark:          $gray-800 !default;
